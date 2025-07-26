fc41de9f1319ce1f42ceb92b3c8f56a9794a2563
amhimel <amhimeltech@gmail.com>
2025-07-26 20:51:48 +0600

    Splash Screen work fine

---
+++ android/app/src/main/kotlin/com/am/himel/github/expense_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.am.himel.github.expense_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,89 @@
+import 'package:expense_tracker/screens/home.dart';
+import 'package:expense_tracker/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:lottie/lottie.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadSplash();
+  }
+  // Load the splash screen for some duration
+  void loadSplash() async {
+    print("passing..");
+    await Future.delayed(Duration(seconds: 3));
+    print("splashing..");
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //this is our dark mode theme
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardTheme.of(context).copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      //this is our light mode theme
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme.of(context).copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+            color: kColorScheme.onSecondaryContainer,
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: HomeScreen()
+      ),
+
+
+    );
+  }
+}
